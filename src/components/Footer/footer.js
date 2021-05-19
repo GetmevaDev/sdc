1aed5f2a7205945b20f4f71ed69458933a31c3dd
GetmevaDev <devteam@getmeva.com>
2021-05-19 11:46:21 +0500

    add link

--- src/components/Footer/footer.js
+++ src/components/Footer/footer.js
@@ -4,6 +4,7 @@ import SocialMediaFooter from "./SocialMediaFooter/SocialMediaFooter"
 import classes from "./footer.module.scss"
 import ContactFooter from "./ContactFooter/ContactFooter"
 import LogoFooter from "./LogoFooter/LogoFooter"
+import LogoM from "../../images/111.svg"
 
 
 
@@ -19,14 +20,28 @@ export default function Footer(){
 
   return(
     <footer className={classes.footer}>
-      <div className={`container row ${classes.flexStyles}`} style={{
+      <div className={`container`} style={{
         paddingTop: 100,
         paddingBottom: 100
       }}>
-        <SocialMediaFooter />
-        <LogoFooter />
-        <ContactFooter />
+        <div className={`row ${classes.flexStyles}`}>
+          <SocialMediaFooter />
+          <LogoFooter />
+          <ContactFooter />
+        </div>
+        <div className={`row manufacturer`}>
+          <div className={`row adaptive-m`} style={{
+            alignItems: `center`,
+            marginLeft: 55,
+            marginTop: 23,
+          }}>
+            <img src={LogoM} alt="" />
+            <p>Web Design & Digital Marketing <br/>
+              by <a href="">Robert Gerov Media</a></p>
+          </div>
+        </div>
       </div>
+
       <p className={classes.lastP} style={{
         textAlign: `center`,
         background: `#212121`,
