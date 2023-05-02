8b6deae175ff4acf10a59d5eff29846ef4301b37
Arsen <arsen@getmeva.com>
2023-05-02 12:31:11 +0500

    add google scripts

--- src/html.js
+++ src/html.js
@@ -5,6 +5,16 @@ export default function HTML(props) {
   return (
     <html {...props.htmlAttributes}>
     <head>
+      <script async src="https://www.googletagmanager.com/gtag/js?id=G-DNLH0SVLZ7"></script>
+      <script
+      dangerouslySetInnerHTML={{
+        __html: `window.dataLayer = window.dataLayer || [];
+        function gtag(){dataLayer.push(arguments);}
+        gtag('js', new Date());
+
+        gtag('config', 'G-DNLH0SVLZ7');`
+      }}
+      />
       <meta charSet="utf-8" />
       <meta httpEquiv="x-ua-compatible" content="ie=edge" />
       <meta
