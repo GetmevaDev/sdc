c5b983b1eb11dbedcffc7297adacc6f34beb8b17
Arsen <arsen@getmeva.com>
2023-06-08 08:42:15 +0500

    add sitemap

--- gatsby-config.js
+++ gatsby-config.js
@@ -13,6 +13,7 @@ module.exports = {
         policy: [{ userAgent: "*", allow: "/" }],
       },
     },
+    `gatsby-plugin-sitemap`,
     {
       resolve: `gatsby-source-strapi`,
       options: {
